56e77d28ab614eaa8538eed31d1735a8099e2c72
lxcj <1714997+lxcj@users.noreply.github.com>
2026-02-25 00:52:59 +0100

    update fish config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,8 +1,22 @@
 if status is-interactive
-# Commands to run in interactive sessions can go here
+  # Commands to run in interactive sessions can go here
+  alias cat 'bat --theme="Dracula"'
+  alias ls 'ls --color=auto'
+  alias ll 'ls -l'
+  alias la 'ls -a'
+  alias lla 'ls -la'
+  alias grep rg
+  alias find fd
+  alias tr tree
+  alias ff fastfetch
+  alias v nvim
 end
 
-set fish_greeting ""
+set fish_greeting
+bind tab accept-autosuggestion
+
+export TERM=xterm-256color
 
 starship init fish | source
+mise activate fish | source
 
