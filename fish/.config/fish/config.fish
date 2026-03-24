5017f7e45033c90a6157667500e5c10f88b53d7c
lxcj <1714997+lxcj@users.noreply.github.com>
2026-03-24 21:20:44 +0100

    switch back to fish with updated config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,27 +1,36 @@
 if status is-interactive
-  # Commands to run in interactive sessions can go here
-  alias cat 'bat --theme="Dracula"'
-  alias ls 'ls --color=auto'
-  alias ll 'ls -l'
-  alias la 'ls -a'
-  alias lla 'ls -la'
-  alias grep rg
-  alias find fd
-  alias tr tree
-  alias ff fastfetch
-  alias v nvim
+    # Commands to run in interactive sessions can go here
 end
 
 set fish_greeting
-
 export TERM=xterm-256color
 
-starship init fish | source
+# Set PATH
+set -gx PATH "$HOME/.bun/bin" $PATH
+set -gx PATH "$HOME/go/bin" $PATH
+set -gx PATH "$HOME/.config/composer/vendor/bin" $PATH
+
+# Ruby
+if type -q ruby && type -q gem
+    # Get the user‑gem directory (e.g. /home/user/.gem)
+    set -l gem_dir (ruby -r rubygems -e 'puts Gem.user_dir')
+    # Prepend its bin sub‑directory to PATH
+    set -gx PATH "$gem_dir/bin" $PATH
+end
+
+# Aliases
+alias cat 'bat --theme="Dracula"'
+alias ls 'eza --icons --group-directories-first -F'
+alias grep rg
+alias find fd
+alias hx helix
+alias z zellij
+
+# mise
 mise activate fish | source
 
 # Functions
 function serve
-  echo "Serving on http://localhost:7777"
-  php -S 0.0.0.0:7777
+    echo "Serving on http://localhost:7777"
+    php -S 0.0.0.0:7777
 end
-
