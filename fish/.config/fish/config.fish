e29f9fc9e51213535a28a30e0a1eb60195262ace
lxcj <1714997+lxcj@users.noreply.github.com>
2026-04-07 00:04:38 +0200

    update

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,16 +2,22 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+# Disable greeting
 set fish_greeting
+
+# Set terminal colors
 export TERM=xterm-256color
+
+# Set default editor
 export EDITOR=helix
 
 # Set PATH
+set -gx PATH "$HOME/.local/bin/" $PATH
 set -gx PATH "$HOME/.bun/bin" $PATH
 set -gx PATH "$HOME/go/bin" $PATH
 set -gx PATH "$HOME/.config/composer/vendor/bin" $PATH
 
-# Ruby
+# Add Ruby gems to PATH
 if type -q ruby && type -q gem
     # Get the user‑gem directory (e.g. /home/user/.gem)
     set -l gem_dir (ruby -r rubygems -e 'puts Gem.user_dir')
