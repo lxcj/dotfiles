ceef619685db5cde902b86fad724f67d3b2c04cc
lxcj <1714997+lxcj@users.noreply.github.com>
2026-07-31 16:08:33 +0000

    git config

--- modules/git.nix
+++ modules/git.nix
@@ -3,5 +3,9 @@
   programs.git = {
     enable = true;
     settings.init.defaultBranch = "main";
+    settings.user = {
+      name = "lxcj";
+      email = "1714997+lxcj@users.noreply.github.com";
+    };
   };
 }
