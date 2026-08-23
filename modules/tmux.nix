a96fac4d5381301e5d3f833b79d995b25e6833e0
lxcj <1714997+lxcj@users.noreply.github.com>
2026-08-23 23:50:17 +0000

    tmux: add fzf session switcher popup

--- modules/tmux.nix
+++ modules/tmux.nix
@@ -19,6 +19,10 @@
       set -g pane-border-style "fg=black"
       set -g extended-keys on
       set -g extended-keys-format csi-u
+
+      # Switch session via fzf popup (prefix + C-s)
+      bind-key C-s display-popup -E \
+        "tmux list-sessions -F '#S' | fzf --reverse --height=100% | xargs tmux switch-client -t"
     '';
   };
 }
